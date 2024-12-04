69ec79fc930a0a9402cff1cf5f28547f7b664738
kenny <mbs220000@utdallas.edu>
2024-12-03 23:02:57 -0600

    Adding More code for screen popup, pygame won't work

--- src/Snake.py
+++ src/Snake.py
@@ -8,13 +8,21 @@ def main():
     pygame.display.set_caption("Snake Game")
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        black = pygame.Color(0, 0, 0)
+        screen.fill(black)
+        pygame.display.flip()
     pygame.quit()
 
-class Snake():
-    def snakespawn():
+#class Snake():
 
-class Fruit():
-    def fruitspawn():
+    #def snakespawn():
+#class Fruit():
+    #def fruitspawn():
 
 #Spawn Snake
 
@@ -25,6 +33,3 @@ class Fruit():
 #Movement (Wasd)
 
 #Snake Grows
-
-
-
