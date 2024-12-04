b4f0439772d151d635f4e5cbfbbeefa2f15592ea
kenny <mbs220000@utdallas.edu>
2024-12-03 22:52:25 -0600

    Trying to get main popup screen

--- src/Snake.py
+++ src/Snake.py
@@ -1,6 +1,20 @@
 #Import Pygame
+import random
+import pygame
 
 #Set Window Size/ Popup Screen
+def main():
+    pygame.init()
+    pygame.display.set_caption("Snake Game")
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    pygame.quit()
+
+class Snake():
+    def snakespawn():
+
+class Fruit():
+    def fruitspawn():
 
 #Spawn Snake
 
