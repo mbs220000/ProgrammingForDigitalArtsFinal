08789052845ef9415bdf7845cf984508ba475a91
kenny <mbs220000@utdallas.edu>
2024-12-05 00:48:03 -0600

    clean up colliders and colors

--- src/Snake.py
+++ src/Snake.py
@@ -9,12 +9,19 @@ distance_x, distance_y = 800, 600
 screen = pygame.display.set_mode((distance_x, distance_y))
 pygame.display.set_caption("Snake Game")
 
+#Basic Colors
 Black = pygame.Color (0, 0, 0)
 White = pygame.Color (255, 255, 255)
 Red = pygame.Color (255, 0, 0)
 Green = pygame.Color (0, 255, 0)
 Blue = pygame.Color (0, 0, 255)
 Grey = pygame.Color (128,128,128)
+
+#My Colors
+PrettyRed = pygame.Color (170, 50, 50)
+PrettyGrey = pygame.Color (140, 145, 140)
+PrettyBlue = pygame.Color (50, 130, 170)
+
 
 clock = pygame.time.Clock()
 
@@ -31,7 +38,7 @@ change_to = snake_direction
 
 def snake(snake_size, snake_body):
     for x in reversed (snake_body):
-        pygame.draw.rect(screen, Black, [x[0], x[1], snake_size, snake_size])
+        pygame.draw.rect(screen, PrettyBlue, [x[0], x[1], snake_size, snake_size])
         #pygame.draw.rect(screen, Green, [x[0] + 1, x[1] + 1, snake_size - 2, snake_size - 2])
         
 #Spawn/ Despawn Fruit
@@ -40,7 +47,7 @@ fruit_position = [random.randrange(1, distance_x // 10) * 10,
 
 def fruit():
     #print(f"Fruit position: {fruit_position}")  # Check the fruit's position
-    pygame.draw.rect(screen, Red, [fruit_position[0], fruit_position[1], snake_size, snake_size])
+    pygame.draw.rect(screen, PrettyRed, [fruit_position[0], fruit_position[1], snake_size, snake_size])
 
 def game_score(score):
     value = score_font.render("Your Score:" + str(score), True, Black)
@@ -97,21 +104,22 @@ def gameloop():
             snake_direction = 'right'
 #Move Snake
         if snake_direction == 'up':
-            snake_head [1] -= 10
+            snake_head [1] -= snake_speed
         if snake_direction == 'down':
-            snake_head [1] += 10
+            snake_head [1] += snake_speed
         if snake_direction == 'left':
-            snake_head [0] -= 10
+            snake_head [0] -= snake_speed
         if snake_direction == 'right':
-            snake_head [0] += 10
+            snake_head [0] += snake_speed
 
             
 #Snake Body moves with Head and Snake grows
         snake_body.insert(0, list(snake_head))
-        if snake_head[0] == fruit_position[0] and snake_head[1] == fruit_position[1]:
+            #Collider on fruit is bigger so you can just touch it and eat it
+        if abs(snake_head[0] - fruit_position[0]) < snake_size and abs(snake_head[1] - fruit_position[1]) < snake_size:
             score += 1
             fruit_spawn = False
-                #Add 2 segments instead of 1
+                #Add 2 segments instead of 1 when eating fruit
             for _ in range(2):  
                 snake_body.insert(0, list(snake_head))
           #won't grow unless it eats fruit  
@@ -123,12 +131,12 @@ def gameloop():
                               random.randrange(1, distance_y // 10) * 10]
             fruit_spawn = True 
         for pos in snake_body:
-            pygame.draw.rect(screen, Green, pygame.Rect(pos[0], pos[1], 10, 10))
-            pygame.draw.rect(screen, White, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+            pygame.draw.rect(screen, Green, pygame.Rect(pos[0], pos[1], snake_size, snake_size))
+            pygame.draw.rect(screen, White, pygame.Rect(fruit_position[0], fruit_position[1], snake_size, snake_size))
         
         
 #Snake, Fruit, and Score on Screen spawn
-        screen.fill(Grey)
+        screen.fill(PrettyGrey)
         snake(snake_size, snake_body)
         fruit()
         game_score(score) #Shows Score While Playing
