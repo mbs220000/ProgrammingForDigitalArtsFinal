84f22b8c0631834999869924de82ee04575200fc
kenny <mbs220000@utdallas.edu>
2024-12-05 00:17:58 -0600

    Fruit collection and fruit respawns when eaten

--- src/Snake.py
+++ src/Snake.py
@@ -1,16 +1,14 @@
+
 import pygame
 import random
 import time
 
-# Initialize Pygame
 pygame.init()
 
-# Set up display
 distance_x, distance_y = 800, 600
 screen = pygame.display.set_mode((distance_x, distance_y))
 pygame.display.set_caption("Snake Game")
 
-# Colors
 Black = pygame.Color (0, 0, 0)
 White = pygame.Color (255, 255, 255)
 Red = pygame.Color (255, 0, 0)
@@ -18,28 +16,27 @@ Green = pygame.Color (0, 255, 0)
 Blue = pygame.Color (0, 0, 255)
 Grey = pygame.Color (128,128,128)
 
-# Clock
 clock = pygame.time.Clock()
 
-# Spawn Snake
-snake_size = 25
+#Spawn Snake
+snake_size = 40 #must be divisible by 10? for grid
 snake_speed = 15
 snake_head = [400, 300]
-snake_body = [[400, 300],
-              [425, 300],
-              [450, 300],
-              [475, 300]]
+snake_body = [[300, 300],
+              [200, 300],
+              [100, 300]]
 snake_direction = 'right'
 change_to = snake_direction
 
 
-def snake(snake_size, snake_grow):
-    for x in snake_grow:
+def snake(snake_size, snake_body):
+    for x in reversed (snake_body):
         pygame.draw.rect(screen, Black, [x[0], x[1], snake_size, snake_size])
-
-# Spawn/ Despawn Fruit
-fruit_position = [random.randrange(0, distance_x // snake_size) * snake_size, 
-                      random.randrange(0, distance_y // snake_size) * snake_size]
+        #pygame.draw.rect(screen, Green, [x[0] + 1, x[1] + 1, snake_size - 2, snake_size - 2])
+        
+#Spawn/ Despawn Fruit
+fruit_position = [random.randrange(1, distance_x // 10) * 10, 
+                  random.randrange(1, distance_y // 10) * 10]
 
 def fruit():
     #print(f"Fruit position: {fruit_position}")  # Check the fruit's position
@@ -51,13 +48,13 @@ def game_score(score):
 
 score_font = pygame.font.SysFont("times new roman", 25)
 
-def game_over():
-    end_score = (snake_size - (snake_size-1))
+def game_over(score):
+    end_score = score
     endscreen = end_score_font.render("Final Score:" + str(end_score), True, Black)
     game_end = endscreen.get_rect()
     game_end.midtop = (distance_x/2, distance_y/4)
     screen.blit(endscreen, game_end)
-    pygame.display.flip()
+    pygame.display.flip() 
     time.sleep(3)
     pygame.quit()
     quit()
@@ -65,9 +62,11 @@ def game_over():
 end_score_font = pygame.font.SysFont("times new roman", 40)
 
 
-# Main Function fo running the game
+#Main Function fo running the game
 def gameloop():
     global fruit_position, change_to, snake_direction
+    score = 0
+    fruit_spawn = True
     running = True
     #time.sleep(5)  # Add a delay to see if the loop starts
     while running:
@@ -77,17 +76,17 @@ def gameloop():
             if event.type == pygame.QUIT:
                 running = False 
 
-        #Moving the Snake
+#Moving the Snake
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    change_to = 'up'
-                if event.key == pygame.K_DOWN:
-                    change_to = 'down'
-                if event.key == pygame.K_LEFT:
-                    change_to = 'left'
-                if event.key == pygame.K_RIGHT:
-                    change_to = 'right'
-        #Update snake direction
+            if event.key == pygame.K_UP:
+                change_to = 'up'
+            if event.key == pygame.K_DOWN:
+                change_to = 'down'
+            if event.key == pygame.K_LEFT:
+                change_to = 'left'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'right'
+#Update snake direction
         if change_to == 'up' and snake_direction != 'down':
             snake_direction = 'up'
         if change_to == 'down' and snake_direction != 'up':
@@ -96,7 +95,7 @@ def gameloop():
             snake_direction = 'left'
         if change_to == 'right' and snake_direction != 'left':
             snake_direction = 'right'
-        #Move Snake
+#Move Snake
         if snake_direction == 'up':
             snake_head [1] -= 10
         if snake_direction == 'down':
@@ -105,30 +104,45 @@ def gameloop():
             snake_head [0] -= 10
         if snake_direction == 'right':
             snake_head [0] += 10
-        #Snake Body moves with Head
+
+            
+#Snake Body moves with Head and Snake grows
         snake_body.insert(0, list(snake_head))
         if snake_head[0] == fruit_position[0] and snake_head[1] == fruit_position[1]:
-            score += 10
+            score += 1
             fruit_spawn = False
+                #Add 2 segments instead of 1
+            for _ in range(2):  
+                snake_body.insert(0, list(snake_head))
+          #won't grow unless it eats fruit  
         else:
-            snake_body.pop()
+            snake_body.pop() 
+          #When fruit is eaten it despawns and a new one spawns  
+        if not fruit_spawn:
+            fruit_position = [random.randrange(1, distance_x // 10) * 10, 
+                              random.randrange(1, distance_y // 10) * 10]
+            fruit_spawn = True 
+        for pos in snake_body:
+            pygame.draw.rect(screen, Green, pygame.Rect(pos[0], pos[1], 10, 10))
+            pygame.draw.rect(screen, White, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
         
-
-        #Snake, Fruit, and Score on Screen spawn
+        
+#Snake, Fruit, and Score on Screen spawn
         screen.fill(Grey)
         snake(snake_size, snake_body)
         fruit()
-        game_score(snake_size - (snake_size-1)) #Shows Score While Playing
-    
-        #Game Over
-        if snake_head[0] < 0 or snake_head[0] > distance_x-10:
-            game_over()
-        if snake_head[1] < 0 or snake_head[1] > distance_y-10:
-            game_over()
+        game_score(score) #Shows Score While Playing
+
+        
+#Game Over
+        if snake_head[0] < 0 or snake_head[0] > distance_x -10:
+            game_over(score)
+        if snake_head[1] < 0 or snake_head[1] > distance_y -10:
+            game_over(score)
 
 
         pygame.display.update()
-        clock.tick(snake_speed)  # FPS
+        clock.tick(snake_speed) #FPS
 
 gameloop()
 
